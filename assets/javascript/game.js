27272a6e8e6f888f3b24ffc56f7b3747b5de70e8
Mike Kravtsov <mike4040@me.com>
2019-08-30 17:11:20 -0700

    Crystals in place

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
   $('body').append($('<div>').text('kkjk'));
+  // The random number shown at the start of the game should be between 19 - 120.
+
+// Each crystal should have a random hidden value between 1 - 12.
 });
