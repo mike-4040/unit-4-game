cf7561a911f83e3ce7b7e7ec0e4cbb7574d130a3
Mike Kravtsov <mike4040@me.com>
2019-08-31 20:07:31 -0700

    logic complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,44 @@
-$(document).ready(function() {
-  $('body').append($('<div>').text('kkjk'));
-  // The random number shown at the start of the game should be between 19 - 120.
+let goalMin = 19;
+let goalMax = 120;
+let crystalMax = 12;
 
-// Each crystal should have a random hidden value between 1 - 12.
+let wins = 0;
+let losses = 0;
+let score;
+let goal;
+
+$(document).ready(function() {
+  restart();
+
+  $('img').click(function() {
+    score += parseInt($(this).attr('value'));
+    // $(this).animate({opacity: 0.25});
+    $('#score').text(score);
+    console.log(score);
+
+    if (score == goal) {
+      wins++;
+      restart();
+    } else if (score > goal) {
+      losses++;
+      restart();
+    }
+  });
 });
+
+function restart() {
+  $('#wins').text(wins);
+  $('#losses').text(losses);
+
+  // goal = randomInRange(goalMin, goalMax);
+  // score = 0;
+
+  $('#goal').text(goal = randomInRange(goalMin, goalMax));
+  $('#score').text( score = 0);
+
+  $('img').each(function() {
+    $(this).attr('value', randomInRange(1, crystalMax));
+  });
+}
+
+randomInRange = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
